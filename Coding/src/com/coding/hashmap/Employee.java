97db508381a1c442c48304a243f61a3559b84e07
Biswajit Mohanty <bjmohanty22@gmail.com>
2022-08-20 20:59:28 +0530

    Map Sorting By Multiple values

---
+++ Coding/src/com/coding/hashmap/Employee.java
@@ -0,0 +1,35 @@
+package com.coding.hashmap;
+
+public class Employee {
+	private Long id;
+	private String name;
+
+	public Employee(Long id, String name) {
+
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+}
---
+++ Coding/src/com/coding/hashmap/MapSorting.java
@@ -0,0 +1,29 @@
+package com.coding.hashmap;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapSorting {
+	public static void main(String[] args) {
+		Map<String, Employee> map = new HashMap<>();
+
+		Employee employee1 = new Employee(1l, "Mher");
+		map.put(employee1.getName(), employee1);
+		Employee employee2 = new Employee(2l, "Annie");
+		map.put(employee2.getName(), employee2);
+		Employee employee3 = new Employee(8l, "John");
+		map.put(employee3.getName(), employee3);
+		Employee employee4 = new Employee(2l, "George");
+		map.put(employee4.getName(), employee4);
+		
+
+		map.entrySet().stream().sorted(
+				Map.Entry.comparingByValue(
+						Comparator.comparingLong(Employee::getId).
+						thenComparing(Employee::getName)
+				)
+		).forEach(System.out::println);
+		
+	}
+}
